6fdd9e3bf4e7f7b3e65616f402fa5a5b7f71cc8c
Jhon <jhon@jhon.Ienovo>
2022-09-15 22:03:45 -0300

    Configurando ambiente React com typescript para o Front

---
+++ FrontWEB/src/App.tsx
@@ -0,0 +1,25 @@
+interface ButtonProps{
+  title: String;
+}
+
+function Button(props: ButtonProps){
+  return(
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
+function App() {
+
+  return (
+  <div>
+    <Button title="Send 1"/>
+    <Button title="Send 2"/>
+    <Button title="Send 3"/>
+    <Button title="Send 4"/>
+  </div>
+  )
+}
+
+export default App
